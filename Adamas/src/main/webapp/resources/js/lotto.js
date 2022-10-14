3e494a0446e022043c8e82c0c055876cf7c2195e
Olimpico <olim84m@gmail.com>
2022-10-14 16:14:45 +0900

    랜덤발생기 필요없는 alert 제거

--- Adamas/src/main/webapp/resources/js/lotto.js
+++ Adamas/src/main/webapp/resources/js/lotto.js
@@ -14,12 +14,14 @@ $(document).ready(function(){
 			
 			// 일단 history 불러오기
 			for(var i=0; i<history.length; i++){
-				str+="<li>"+history[i].lno+"</li>"
-				str+="<li>"+history[i].firstPrize+"</li>"
-				str+="<li>"+history[i].secondPrize+"</li>"
-				str+="<li>"+history[i].thirdPrize+"</li>"
-				str+="<li>"+history[i].fourthPrize+"</li>"
-				str+="<li>"+history[i].fifthPrize+"</li>"
+				str+="<li>회차 : "+history[i].lno+"</li>"
+				str+="<li>"+history[i].ball1+"</li>"
+				str+="<li>"+history[i].ball2+"</li>"
+				str+="<li>"+history[i].ball3+"</li>"
+				str+="<li>"+history[i].ball4+"</li>"
+				str+="<li>"+history[i].ball5+"</li>"
+				str+="<li>"+history[i].ball6+"</li>"
+				str+="<li>보너스 : "+history[i].bonus+"</li>"
 			}
 			
 			$(".compare").html(str);
--- Adamas/src/main/webapp/resources/js/randomGenerator.js
+++ Adamas/src/main/webapp/resources/js/randomGenerator.js
@@ -37,26 +37,26 @@ $(document).ready(function(){
 		
 		if($(this).is(":checked")){		// checkbox 체크 될 때
 			if(fixedBtn == 1){			// 고정수 버튼 활성화 상태
-				alert("고정수 상태");
+				//alert("고정수 상태");
 				if(fixedNum.length > 4){	
 					$(this).prop("checked", false);
 					alert("고정수는 5개까지만 선택 가능합니다");	
 					
 				} else{
-					alert("고정수 체크한다"+$(this).val());
+					//alert("고정수 체크한다"+$(this).val());
 					fixedNum.push($(this).val());
 					fixedNum.sort(function(a, b) {return a-b;});
 					
 				}
 				
 			} else if(excludedBtn == 1){	// 제외수 버튼 활성화 상태
-				alert("제외수 상태");
+				//alert("제외수 상태");
 				if(excludedNum.length > 34){
 					$(this).prop("checked", false);
 					alert("제외수는 35개까지만 선택 가능합니다");
 					
 				} else{
-					alert("제외수 체크한다"+$(this).val());
+					//alert("제외수 체크한다"+$(this).val());
 					excludedNum.push($(this).val());
 					excludedNum.sort(function(a, b) {return a-b;});
 					
@@ -68,7 +68,7 @@ $(document).ready(function(){
 			}
 			
 		} else{							// checkbox 체크 해제 될 때
-			alert("체크해제한다"+$(this).val());
+			//alert("체크해제한다"+$(this).val());
 				fixedNum = fixedNum.filter(item => item != $(this).val());
 				excludedNum = excludedNum.filter(item => item != $(this).val());
 			
@@ -90,8 +90,8 @@ $(document).ready(function(){
 		
 		$("#fixedNum").html(fixedStr);
 		$("#excludedNum").html(excludedStr);
-		alert("고정수 선택상태"+fixedNum);
-		alert("제외수 선택상태"+excludedNum);
+		//alert("고정수 선택상태"+fixedNum);
+		//alert("제외수 선택상태"+excludedNum);
 	})
 	
 	// 초기화 버튼을 눌렀을 때
@@ -100,15 +100,15 @@ $(document).ready(function(){
 		fixedNum = [];
 		excludedNum = [];
 		
-		alert("초기화합니다");
+		//alert("초기화합니다");
 		
 		$("input[type='checkbox']:checked").each(function(){
 			$(this).prop("checked", false);
 		})
 		
 		$(".checkedNum").html(str);
-		alert("고정수"+fixedNum);
-		alert("제외수"+excludedNum);
+		//alert("고정수"+fixedNum);
+		//alert("제외수"+excludedNum);
 	})
 	
 	// 번호 생성 버튼을 눌렀을 때
@@ -116,7 +116,7 @@ $(document).ready(function(){
 		
 		// 랜덤 + 고정수 번호를 저장할 배열
 		var lotto = [];
-		alert("번호 발생 전 로도번호"+lotto);
+		//alert("번호 발생 전 로도번호"+lotto);
 		// 반복문을 위한 변수
 		var count = 0;
 		// 번호중복, 제외수 방지용 변수
@@ -124,7 +124,7 @@ $(document).ready(function(){
 		//
 		var str = "";
 		
-		alert("고정수 배열 길이"+fixedNum.length);
+		//alert("고정수 배열 길이"+fixedNum.length);
 		
 		// (6-고정수)개의 로또번호를 얻기위한 반복문
 		while(count < 6-fixedNum.length){
@@ -163,7 +163,7 @@ $(document).ready(function(){
 			prevent = true;
 		}
 
-		alert("발생 후"+lotto);
+		//alert("발생 후"+lotto);
 		// 발생 한 로또 번호와 고정 수를 합친다
 		lotto.push(...fixedNum);
 		lotto.sort(function(a, b) {return a-b;});
@@ -176,7 +176,7 @@ $(document).ready(function(){
 		}
 		
 		$("#lotto").html(str);
-		alert("발생 수와 고정 수 합친 후 : "+lotto);
+		//alert("발생 수와 고정 수 합친 후 : "+lotto);
 	})
 	
 })
