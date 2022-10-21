2377759fe6edba9aad74d0045c6f0d1631b43fde
Olimpico <olim84m@gmail.com>
2022-10-21 16:13:12 +0900

    당첨 판매점(약간의 테스트)

--- Adamas/src/main/java/org/adamas/mapper/StoreMapper.java
+++ Adamas/src/main/java/org/adamas/mapper/StoreMapper.java
@@ -9,6 +9,7 @@ public interface StoreMapper {
 
 	// 판매점 리스트 DB설계
 	public ArrayList<StoreVO> storeList();
+	
 	// 1등 당첨 판매점 리스트 DB설계
 	public ArrayList<TopStoreVO> topStoreList();
 }
--- Adamas/src/main/java/org/adamas/service/StoreService.java
+++ Adamas/src/main/java/org/adamas/service/StoreService.java
@@ -9,6 +9,7 @@ public interface StoreService {
 
 	// 판매점 리스트 설계
 	public ArrayList<StoreVO> storeList();
+	
 	// 1등 당첨 판매점 리스트
 	public ArrayList<TopStoreVO> topStoreList();
 }
